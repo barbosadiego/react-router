33af56d9e8beecce373b94a9da385609285707a8
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-17 15:24:28 -0300

    aula 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Contacts from './components/Contacts';
+import Home from './components/Home';
+import Services from './components/Services';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>React Router</h1>
-    </div>
+      <Routes>
+        <Route path='/' element={ <Home />}/>
+        <Route path='/services' element={ <Services />}/>
+        <Route path='/contacts' element={ <Contacts />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contacts = (props) => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+    </div>
+  )
+}
+
+export default Contacts
