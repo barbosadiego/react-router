03870cc9e0c8cab983d1afd5e412b4c44390e029
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-18 11:31:23 -0300

    aula 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
 
-//react router
-import { Route, Routes } from 'react-router-dom';
-
-//bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//css custom
-import './App.css';
+//router
+import { Routes, Route } from 'react-router-dom';
 
 //components
-import Header from './Header';
-import Empresa from './Empresa';
-import Lojas from './Lojas';
-import LisboaLoja from './LisboaLoja';
-import PortoLoja from './PortoLoja';
-import CoimbraLoja from './CoimbraLoja';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <hr />
-      <main>
-        <Routes>
-          <Route path='/' element={ <Empresa />}/>
-          <Route path='/lojas/*' element={ <Lojas /> }>
-            <Route path='lisboa' element={ <LisboaLoja />} />
-            <Route path='porto' element={ <PortoLoja />} />
-            <Route path='coimbra' element={ <CoimbraLoja />} />
-          </Route>
-        </Routes>
-      </main>
+      <h2>Route Index</h2>
+      <Routes>
+        <Route path='/' element={ <Layout /> }>
+          <Route index element={ <Home /> }/>
+          <Route path='/about' element={ <About /> }/>
+          <Route path='/services' element={ <Services /> }/>
+        </Route>
+      </Routes>
     </>
   );
 };
