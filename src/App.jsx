bd8541073d4f8ce45b041de9d25e62a72b823924
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-18 09:50:14 -0300

    aula 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import React from 'react';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
-import Contacts from './components/Contacts';
-import Home from './components/Home';
-import Services from './components/Services';
-import Erro from './components/Erro';
-import About from './components/About';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AreaReservada from './components/AreaReservada';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-
-  const [logado, setLogado] = useState(false);
-
   return (
     <>
-      <Header setLogado={setLogado}/>
-      {logado ? 'usuário logado' : 'usuário não logado'}
-      <main>
-        <Routes>
-          <Route path='/' element={ <Home />}/>
-          <Route path='/services/*' element={ <Services />}/>
-          <Route path='/contacts' element={ <Contacts />}/>
-          <Route path='/about/:id' element={ <About />} />
-          
-          <Route 
-            path='/area_reservada' 
-            element={ logado ? <AreaReservada /> : <Navigate to='/' /> } 
-          />
+      <h1>React Router!</h1>
+      <nav>
+        <span>
+          <Link to="/">HOME</Link>
+        </span>{' '}
+        <span>
+          <Link to="/about">ABOUT</Link>
+        </span>{' '}
+        <span>
+          <Link to="/contact">CONTACT</Link>
+        </span>
+      </nav>
+      <hr />
 
-          <Route path='*' element={ <Erro />} />
-        </Routes>
-      </main>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<h2>home</h2>} />
+        <Route path="/about" element={<h2>about</h2>} />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <h2>contact</h2>
+              <nav>
+                <Link to="local">local</Link>{' '}
+                <Link to="worktime">worktime</Link>{' '}
+                <Link to="phone">phone</Link> <Link to="email">email</Link>
+              </nav>
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="local" element={<h3>local</h3>} />
+          <Route path="worktime" element={<h3>worktime</h3>} />
+          <Route path="phone" element={<h3>phone</h3>} />
+          <Route path="email" element={<h3>email</h3>} />
+        </Route>
+        <Route path="*" element={<h2>page not found</h2>} />
+      </Routes>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = document.querySelector('#root');
 ReactDOM.createRoot(root).render(
